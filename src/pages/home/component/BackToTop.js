b9e7689967e95b06d2dcae0f95364f3833646a8c
yuhui1998 <1176951680@qq.com>
2021-02-09 15:08:32 +0800

    react项目复习，并添加部分注释

--- src/pages/home/component/BackToTop.js
+++ src/pages/home/component/BackToTop.js
@@ -1,53 +1,57 @@
-import React,{PureComponent} from 'react';
-import {BackToTop} from '../style.js';
-import {actionCreator} from '../store/index.js';
-import {connect} from 'react-redux';
+import React, { PureComponent } from 'react';
+import { BackToTop } from '../style.js';
+import { actionCreator } from '../store/index.js';
+import { connect } from 'react-redux';
 
-class ToTopButton extends PureComponent{
-	render(){
-		return(
+// PureComponent相比于Component，在shouldComponentUpdate阶段多了一层浅比较。
+class ToTopButton extends PureComponent {
+
+	// 子类可以不添加constructor方法，这个方法会被默认添加
+	render() {
+		return (
 			<div>
-				{	
-					this.props.ToTopShow?
-					<BackToTop onClick ={this.handleScrollTop.bind(this)}>
-						<span className="iconfont ToTop">&#xe603;</span>
-					</BackToTop>
-					:null
+				{
+					this.props.ToTopShow ?
+						<BackToTop onClick={this.handleScrollTop.bind(this)}>
+							<span className="iconfont ToTop">&#xe603;</span>
+						</BackToTop>
+						: null
 				}
 			</div>
 		)
 	}
-	
-	componentDidMount(){
-		window.addEventListener('scroll',this.props.changeScrollState);
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.props.changeScrollState);
 	}
-	componentWillUnmount(){
-		window.removeEventListener('scroll',this.props.changeScrollState);
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.props.changeScrollState);
 	}
-	
-	handleScrollTop(){
+
+	handleScrollTop() {
 		window.scrollTo({
-			top:0,
-			behavior:"smooth"
+			top: 0,
+			behavior: "smooth"
 		})
 	}
 }
 
 
-const mapStoreToProps = (state)=>{
-	return{
-		ToTopShow:state.get("home").get("ToTopShow")
+const mapStoreToProps = (state) => {
+	return {
+		ToTopShow: state.get("home").get("ToTopShow")
 	}
 }
 
-const mapDispatchToStore = (dispatch)=>({
-		changeScrollState(){
-			if(document.documentElement.scrollTop>500){
-				dispatch(actionCreator.toggle(true))	
-			}else{
-				dispatch(actionCreator.toggle(false))	
-			}
-		}	
+const mapDispatchToStore = (dispatch) => ({
+	changeScrollState() {
+		if (document.documentElement.scrollTop > 500) {
+			dispatch(actionCreator.toggle(true))
+		} else {
+			dispatch(actionCreator.toggle(false))
+		}
+	}
 })
 
-export default connect(mapStoreToProps,mapDispatchToStore)(ToTopButton);
+// UI组件外面包一层容器组件
+export default connect(mapStoreToProps, mapDispatchToStore)(ToTopButton);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,15 @@
-import {combineReducers} from 'redux-immutable';
+// Immutable Data 就是一旦创建，就不能再被更改的数据。对 Immutable 对象的任何修改或添加删除操作都会返回一个新的 Immutable 对象。
+import { combineReducers } from 'redux-immutable';
+
 //将reducer进行拆分，避免冗长的代码
-import {reducer as headerReducer} from '../common/headerComponent/store';
-import {reducer as homeReducer} from '../pages/home/store'; 
-import {reducer as detailReducer} from '../pages/detail/store'; 
-import {reducer as LoginIn} from '../pages/login/store'; 
+import { reducer as headerReducer } from '../common/headerComponent/store';
+import { reducer as homeReducer } from '../pages/home/store';
+import { reducer as detailReducer } from '../pages/detail/store';
+import { reducer as LoginIn } from '../pages/login/store';
 
 export default combineReducers({
-	header:headerReducer,
-	home:homeReducer,
-	article:detailReducer,
-	login:LoginIn
+	header: headerReducer,
+	home: homeReducer,
+	article: detailReducer,
+	login: LoginIn
 })
